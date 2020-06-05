bee83b71f34d775d7487dffde021246aec337473
habedi71 <habedi71@gmail.com>
2020-06-06 01:05:46 +0430

    Updates

---
+++ precache-manifest.c3ee2e341b6c3991fb5f0cba75e2746f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8101ce98255d1339e694d53a3fb34108",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "d9f27c9e1ac1c669911c",
+    "url": "/react-tictactoe/static/css/main.71449124.chunk.css"
+  },
+  {
+    "revision": "15e4c428f25ee787cd98",
+    "url": "/react-tictactoe/static/js/2.9b4aafe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.9b4aafe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f27c9e1ac1c669911c",
+    "url": "/react-tictactoe/static/js/main.9d9a50f9.chunk.js"
+  },
+  {
+    "revision": "a5aff2ec57301e49134b",
+    "url": "/react-tictactoe/static/js/runtime-main.92dfbc25.js"
+  }
+]);
